fe8584eee8af4942cdcbc38a7ba6738e9c752b5a
Mehdi Ghodsizadeh <mehdi.ghodsizadeh@gmail.com>
2020-04-18 16:11:23 +0430

    Add Pomodoro basics

--- src/pomodoro/index.js
+++ src/pomodoro/index.js
@@ -11,27 +11,77 @@ const useStyles = makeStyles((theme: Theme) => ({
     borderRadius: "30px",
     // backgroundColor: "red",
   },
+  focus: {
+    backgroundColor: "#af0812",
+    margin: "10px",
+  },
+  buttons: {
+    paddingTop: "300px",
+    margin: "10px",
+    display: "flex",
+  },
 }));
+
+type toggleProps = {
+  title: string,
+  handler: () => void,
+  className: {},
+};
 
 type kinds = "focus" | "shortBreak" | "longBreak";
 export default function Pomodoro() {
   const classes = useStyles();
   const [kind: kinds, setKind] = useState("focus");
+  const [time: number, setTime] = useState(1);
   return (
     <Paper elevation={10} className={classes.root}>
-      <div style={{ paddingLeft: "70px", paddingTop: "30px" }}>
-        <Timer minute={1} kind={kind} />
+      <div style={{ paddingLeft: "80px", paddingTop: "30px" }}>
+        <Timer minute={time} kind={kind} />
       </div>
-      <Button variant="contained" onClick={handleKindClick}>
-        Hello
-      </Button>
+      <div className={classes.buttons}>
+        <ToggleButton
+          className={classes.focus}
+          title="focus"
+          handler={handleFocus}
+        />
+        <ToggleButton
+          className={classes.focus}
+          title="break"
+          handler={handleBreak}
+        />
+        <ToggleButton
+          className={classes.focus}
+          title="long break"
+          handler={handleLongBreak}
+        />
+      </div>
     </Paper>
   );
-  function handleKindClick() {
-    if (kind === "focus") {
-      setKind("shortBreak");
-    } else if (kind === "shortBreak") {
-      setKind("focus");
-    }
+
+  function ToggleButton(props: toggleProps) {
+    const { handler, title, className } = props;
+    return (
+      <Button
+        className={className}
+        variant="contained"
+        color="inherit"
+        onClick={handler}
+      >
+        {title}
+      </Button>
+    );
+  }
+
+  function handleFocus() {
+    setKind("focus");
+    setTime(25);
+  }
+  function handleBreak() {
+    setKind("shortBreak");
+    setTime(5);
+  }
+  function handleLongBreak() {
+    setKind("longBreak");
+    setTime(15);
   }
 }
